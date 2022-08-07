dfa68d1cdce329dbeaa7e4d15290787fcb1b20bf
Jonghan Park <jonghan.park@edu.sait.ca>
2022-08-07 16:51:26 -0600

    Udpate backend model and router

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
+const connection = require("./db/db");
+
+// Router
+const userRoute = require("./routes/user");
+
+// Database
+connection();
+
+app.use(express.json());
+app.use("/api/users", userRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
