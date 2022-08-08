c4fff357565a513ae2e55ad5d998b18d109656dc
Jonghan Park <jonghan.park@edu.sait.ca>
2022-08-07 21:22:25 -0600

    Update cart and orders api

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const connection = require("./db/db");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 // Database
 connection();
@@ -16,6 +18,8 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
--- routes/cart.js
+++ routes/cart.js
@@ -1,3 +1,64 @@
 const router = require("express").Router();
+const Cart = require("../models/Cart");
+const {
+  verifyToken,
+  verifyTokenAuthorization,
+  verifyTokenAdmin,
+} = require("./verifyToken");
+
+// Create
+router.post("/", verifyToken, async (req, res) => {
+  const newCart = new Product(req.body);
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Update
+router.put("/:id", verifyTokenAuthorization, async (req, res) => {
+  try {
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+// Delete
+router.delete("/:id", verifyTokenAuthorization, async (req, res) => {
+  try {
+    await Cart.findByIdAndDelete(req.params.id);
+    res.status(200).json("Cart has been deleted...");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+// Get cart
+router.get("/find/:userId", verifyTokenAuthorization, async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ userId: req.params.userId });
+    res.status(200).json(cart);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get all
+router.get("/", verifyTokenAdmin, async (req, res) => {
+  try {
+    const carts = await Cart.find();
+    res.status(200).json(carts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
